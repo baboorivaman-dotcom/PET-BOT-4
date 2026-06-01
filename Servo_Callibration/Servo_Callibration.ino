77456807aef63845e2735e440ca225713346aa82
ishanjee620-sketch <ishanjee620@gmail.com>
2026-06-01 12:53:16 +0530

    Refactor: Move servo calibration logic to setup() to prevent continuous hardware strain in loop()

--- Servo_Callibration/Servo_Callibration.ino
+++ Servo_Callibration/Servo_Callibration.ino
@@ -1,19 +1,22 @@
-
 #include <Servo.h>   
 
 Servo servo[4][3];
 
-//define servos' ports
-const int servo_pin[4][3] = { {3, 4, 2}, {6, 7, 5}, {9, 8, 10},{12, 11, 13} };
+// Define servos' ports
+const int servo_pin[4][3] = { {3, 4, 2}, {6, 7, 5}, {9, 8, 10}, {12, 11, 13} };
 
 void setup()
 {
-  //initialize all servos
+  // Initialize all servos and set them to the calibration position (90 degrees)
   for (int i = 0; i < 4; i++)
   {
     for (int j = 0; j < 3; j++)
     {
       servo[i][j].attach(servo_pin[i][j]);
+      delay(20);
+      
+      // Move servo to default calibration angle immediately after attaching
+      servo[i][j].write(90);
       delay(20);
     }
   }
@@ -21,12 +24,6 @@ void setup()
 
 void loop(void)
 {
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      servo[i][j].write(90);
-      delay(20);
-    }
-  }
+  // Keeping loop empty because calibration only needs to run once during setup.
+  // This prevents continuous servo writing and hardware strain.
 }
